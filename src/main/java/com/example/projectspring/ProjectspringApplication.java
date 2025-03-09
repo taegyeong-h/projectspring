42ab1f055c5e428d586f42d5b56d0f586b9e008f
taegyeong_h <a01026676497@gmail.com>
2025-03-09 14:06:43 +0900

    Initial commit

---
+++ src/main/java/com/example/projectspring/ProjectspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectspringApplication.class, args);
+	}
+
+}
